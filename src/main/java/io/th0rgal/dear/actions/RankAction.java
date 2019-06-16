0126bd7da130a23221e129dd57d4a0aa509add41
Thomas Marchand <thomas.marchand44@gmail.com>
2019-06-16 18:19:13 +0200

    rank command fixed

--- src/main/java/io/th0rgal/dear/actions/RankAction.java
+++ src/main/java/io/th0rgal/dear/actions/RankAction.java
@@ -2,6 +2,7 @@ package io.th0rgal.dear.actions;
 
 import io.th0rgal.dear.utils.embeds.EmbedUtils;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 public class RankAction implements CommandAction {
@@ -10,18 +11,25 @@ public class RankAction implements CommandAction {
     public void performActions(GuildMessageReceivedEvent event, String command, String[] args) {
         Guild guild = event.getGuild();
 
+        String message = "You successfully received your rank";
+
         switch (args[1].toLowerCase()) {
 
             case "developer":
-
-                guild.addRoleToMember(event.getMember(), guild.getRoleById(588733691463991317L)).submit();
+                Role role = guild.getRoleById(588733691463991317L);
+                if (guild.getMembersWithRoles(role).contains(event.getMember())) {
+                    guild.removeRoleFromMember(event.getMember(), role).submit();
+                    message = "The role has been successfully removed";
+                } else {
+                    guild.addRoleToMember(event.getMember(), role).submit();
+                }
                 break;
 
             default:
-                event.getChannel().sendMessage(EmbedUtils.getEmbedFormating("The requested rank doesn't exist")).queue();
-                return;
+                message = "The requested rank doesn't exist";
+                break;
         }
-        event.getChannel().sendMessage(EmbedUtils.getEmbedFormating("You successfully received your rank")).queue();
+        event.getChannel().sendMessage(EmbedUtils.getEmbedFormating(message)).queue();
     }
 
 }
